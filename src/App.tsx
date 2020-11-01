9781e5cfef892077a8027dfcf749af1defb84474
Carl Orre <carl.orre@gmail.com>
2020-11-01 01:52:56 +0100

    added some styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import Todos from './components/Todos';
 import TodoInput from './components/TodoInput';
+import styled from "styled-components";
+import { Reset } from 'styled-reset'
+
+const AppContainer = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 const App:React.FC = () => (
-  <div>
+  <AppContainer>
+    <Reset />
     <TodoInput/>
     <Todos/>
-  </div>
+  </AppContainer>
 );
  
 export default App;
--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -1,21 +1,29 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addTodo } from "../actions";
+import styled from "styled-components";
+
+const TodoInputContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
 
 const TodoInput = () => {
   const [todo, setTodo] = useState("");
   const dispatch = useDispatch()
   const handleChange = (e:ChangeEvent<HTMLInputElement>) => setTodo(e.target.value);
-  const handleClick = () => {
+  const handleSubmit = (event:FormEvent<HTMLFormElement>) => {
+   
+    event.preventDefault();
     if (!todo) return
     dispatch(addTodo(todo))
     setTodo("")
   }
   return (
-    <div>
-      <input onChange={handleChange} type="text" name="todos" value={todo}/>
-      <button onClick={handleClick}>Add Todo</button>
-    </div>
+    <TodoInputContainer onSubmit={(event) => handleSubmit(event)}>
+        <input type="text" onChange={handleChange} name="todos" value={todo}/>
+        <input type="submit" value="Add todo" />
+    </TodoInputContainer>
   );
 }
 
